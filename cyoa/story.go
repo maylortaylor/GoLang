1d541ff655fe2cc9b98d1deba82c7656e8910c1d
maylortaylor <maylortaylor@gmail.com>
2019-12-30 16:53:33 -0500

    rendered basic html and json parsing

--- cyoa/story.go
+++ cyoa/story.go
@@ -2,8 +2,54 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose Your Own Adventure</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+            <p>{{.}}</p>
+        {{end}}
+
+        <ul>
+            {{range .Options}}
+            <li>
+                <a href="/{{.Chapter}}">{{.Text}}</a>
+            </li>
+            {{end}}
+        </ul>
+    </body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
